a47d0b6e960ce9df427d6d6c2a83f0b8b2d2efc1
MadushikaPerera <madushikad.perera@gmail.com>
2016-05-12 09:48:07 +0530

    Need to fix some issues

--- app/src/main/java/com/sliit/orange/facebookshareingapp/MainActivity.java
+++ app/src/main/java/com/sliit/orange/facebookshareingapp/MainActivity.java
@@ -1,53 +1,31 @@
 package com.sliit.orange.facebookshareingapp;
 
-import android.app.Dialog;
 import android.content.Intent;
-import android.content.res.Configuration;
-import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Matrix;
-import android.media.ExifInterface;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Base64;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.Window;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.ImageView;
-
-import com.bumptech.glide.Glide;
+import android.widget.TextView;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.share.model.ShareLinkContent;
+import com.facebook.share.widget.ShareDialog;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final int ActivityStartCAM=0;
-    private String ImageFileLoaction="";
-    private String bit64camimage="";
-
-    ImageView iv;
-    Dialog d;
-    Button btn1,btn2;
-    private final static int PICK_IMAGE=1;
-    Uri imageUri;
-    String dimagepath="";
-
-    boolean Cam=false;
-    boolean Gal=false;
+    private ShareDialog shareDialog;
+    private Button sharedbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,117 +34,55 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        iv = (ImageView)findViewById(R.id.image1);
-        btn1 = (Button)findViewById(R.id.btnUpload);
-        btn2 = (Button)findViewById(R.id.btnShare);
+        sharedbtn = (Button)findViewById(R.id.btnfbshared);
 
-        btn1.setOnClickListener(new View.OnClickListener() {
+        Bundle inBundle = getIntent().getExtras();
+        String name = inBundle.get("name").toString();
+        String surname = inBundle.get("surname").toString();
+        String imageUrl = inBundle.get("imageUrl").toString();
 
-            @Override
-            public void onClick(View v) {
+        TextView nameView = (TextView)findViewById(R.id.nameAndSurname);
+        nameView.setText("" + name + " " + surname);
 
-                Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
-                startActivityForResult(gallery, PICK_IMAGE);
-                Cam = false;
-                Gal = true;
-                //d.cancel();
-            }
-        });
+        new DownloadImage((ImageView)findViewById(R.id.profileImage)).execute(imageUrl);
 
+        shareDialog = new ShareDialog(this);
 
+    }
+
+    public void sharedPost(View v)
+    {
+        ShareLinkContent content = new ShareLinkContent.Builder().build();
+        shareDialog.show(content);
     }
 
     @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-
-        if(Cam){
-            rotateImage(setReducedImageSize());
-        }else if(Gal){
-            //CIV.setImageURI(imageUri);
-            Glide.with(this).load(imageUri).into(iv);
-        }else{
-
-        }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_login, menu);
+        return true;
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode==PICK_IMAGE  && resultCode==RESULT_OK){
-            imageUri=data.getData();
-            bit64camimage=BitmaptoString(getRealPathFromURI(imageUri));
-            // CIV.setImageURI(imageUri);
-            Glide.with(this).load(imageUri).into(iv);
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
 
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            logout();
+            return true;
         }
-        if(requestCode==ActivityStartCAM  && resultCode==RESULT_OK){
-            bit64camimage=BitmaptoString(ImageFileLoaction);
-            rotateImage(setReducedImageSize());
-            //Toast.makeText(getApplicationContext(), bit64camimage, Toast.LENGTH_SHORT).show();
-        }
+
+        return super.onOptionsItemSelected(item);
     }
 
-    private Bitmap setReducedImageSize(){
-        int taretimagevieweidth=iv.getWidth();
-        int targetimageviewheight=iv.getHeight();
-        BitmapFactory.Options bmfop= new BitmapFactory.Options();
-        bmfop.inJustDecodeBounds=true;
-        BitmapFactory.decodeFile(ImageFileLoaction,bmfop);
-        int Cameraheight=bmfop.outHeight;
-        int Camerawidth=bmfop.outWidth;
-        int scalefactor =Math.min(Camerawidth/taretimagevieweidth,Cameraheight/targetimageviewheight);
-        bmfop.inSampleSize=scalefactor;
-        bmfop.inJustDecodeBounds=false;
-        return  BitmapFactory.decodeFile(ImageFileLoaction,bmfop);
-    }
-
-    private void rotateImage(Bitmap bitmap){
-        ExifInterface exifInterface = null;
-        try{
-            exifInterface = new ExifInterface(ImageFileLoaction);
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-        int orientation = exifInterface.getAttributeInt(ExifInterface.TAG_ORIENTATION,ExifInterface.ORIENTATION_UNDEFINED);
-        Matrix matrix = new Matrix();
-        switch (orientation){
-            case ExifInterface.ORIENTATION_ROTATE_90:
-                matrix.setRotate(90);
-                break;
-            case ExifInterface.ORIENTATION_ROTATE_180:
-                matrix.postRotate(180);
-                break;
-            default:
-
-        }
-        Bitmap rotatedBitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
-        iv.setImageBitmap(rotatedBitmap);
-
-    }
-
-    public String BitmaptoString(String path){
-        // Bitmap bmp = BitmapFactory.decodeResource(getResources(), R.drawable.thara1);
-        Bitmap bmp = BitmapFactory.decodeFile(path);
-        ByteArrayOutputStream stream = new ByteArrayOutputStream();
-        bmp.compress(Bitmap.CompressFormat.JPEG, 50, stream);
-        bmp.recycle();
-        byte[] byteArray = stream.toByteArray();
-        String imageFile = Base64.encodeToString(byteArray, Base64.DEFAULT);
-        return  imageFile;
-    }
-
-    private String getRealPathFromURI(Uri contentURI) {
-        String result;
-        Cursor cursor = getContentResolver().query(contentURI, null, null, null, null);
-        if (cursor == null) { // Source is Dropbox or other similar local file path
-            result = contentURI.getPath();
-        } else {
-            cursor.moveToFirst();
-            int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
-            result = cursor.getString(idx);
-            cursor.close();
-        }
-        return result;
+    public void logout(){
+        LoginManager.getInstance().logOut();
+        Intent login = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(login);
+        finish();
     }
 }
